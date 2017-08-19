6d361ff9d0faf26754545a9aa53ec74c14344fe8
zoe hopkins <zoe.hopkins@gmail.com>
2017-08-19 13:52:23 +0000

    Update the print methods to display a message if user enters no students

--- directory.rb
+++ directory.rb
@@ -13,7 +13,7 @@ def create_new_student
   elsif continue == "n"
     return false
   else
-    puts "Invalid choice".center(@width)
+    puts "Invalid choice - please enter y or n".center(@width)
   end
   create_new_student
 end
@@ -48,60 +48,78 @@ def add_hobbies
 end
 
 def print_header
-  puts "The students of Villains Academy".center(@width)
-  puts "-------------".center(@width)
+  if !@students.empty?
+    puts "The students of Villains Academy".center(@width)
+    puts "-------------".center(@width)
+  else
+  end
 end
 
-# 8.4 
+# 8.12 
 def print(students)
-  i = 0
-  while i < @students.count
-  puts "#{i + 1}. #{@students[i][:name]}, #{@students[i][:country_of_birth]} (#{@students[i][:cohort]})".center(@width)
-  i += 1
+  if students.empty?
+    puts "No students available".center(@width)
+  else
+    i = 0
+    while i < @students.count
+      puts "#{i + 1}. #{@students[i][:name]}, #{@students[i][:country_of_birth]} (#{@students[i][:cohort]})".center(@width)
+      i += 1
+    end
   end
 end
 
 # 8.2
 def print_by_first_letter(students)
-  puts "Student names beginning with: (Please enter a letter)".center(@width)
-  letter = gets.chomp
-  number_of_matches = 0
-  @students.each do |student|
-    if student[:name].start_with?(letter.upcase, letter.downcase)
-      puts "#{student[:name]}, #{student[:country_of_birth]} (#{student[:cohort]} cohort)".center(@width)
-      number_of_matches += 1
-    else
-    end  
+  if @students.empty?
+    puts "No students available".center(@width)
+  else
+    puts "Student names beginning with: (Please enter a letter)".center(@width)
+    letter = gets.chomp
+    number_of_matches = 0
+    @students.each do |student|
+      if student[:name].start_with?(letter.upcase, letter.downcase)
+        puts "#{student[:name]}, #{student[:country_of_birth]} (#{student[:cohort]} cohort)".center(@width)
+        number_of_matches += 1
+      else
+      end  
+    end
+    puts
+    puts "We have #{number_of_matches} students whose name begins with #{letter}".center(@width)
+    puts
   end
-  puts
-  puts "We have #{number_of_matches} students whose name begins with #{letter}".center(@width)
-  puts
 end
 
 # 8.3
 def print_by_name_length(students)
-  puts "Names with maximum characters of: (Please enter a number)".center(@width)
-  max_length = gets.chomp
-  number_of_matches = 0
-  @students.each do |student|
-    if student[:name].length <= max_length.to_i
-      puts "#{student[:name]}, #{student[:country_of_birth]} (#{student[:cohort]} cohort)".center(@width)
-      number_of_matches += 1
-    else
-    end  
+  if @students.empty?
+    puts "No students available".center(@width)
+  else
+    puts "Names with maximum characters of: (Please enter a number)".center(@width)
+    max_length = gets.chomp
+    number_of_matches = 0
+    @students.each do |student|
+      if student[:name].length <= max_length.to_i
+        puts "#{student[:name]}, #{student[:country_of_birth]} (#{student[:cohort]} cohort)".center(@width)
+        number_of_matches += 1
+      else
+      end  
+    end
+    puts
+    puts "We have #{number_of_matches} students with a name of maximum #{max_length} characters".center(@width)
+    puts
   end
-  puts
-  puts "We have #{number_of_matches} students with a name of maximum #{max_length} characters".center(@width)
-  puts
 end
 
 
 def print_footer(students)
-  puts "Overall, we have #{@students.count} great students".center(@width)
+  if !@students.empty?
+    puts "Overall, we have #{@students.count} great students".center(@width)
+  else
+  end
 end
 
 #call the methods
 @students = input_students
-print_header
-print(@students)
+print_header 
+print(@students) 
 print_footer(@students)
