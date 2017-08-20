8bfa06c991077c5d490d673b0a1b027c8064cb00
zoe hopkins <zoe.hopkins@gmail.com>
2017-08-20 13:05:44 +0000

    Add interactive menu

--- directory.rb
+++ directory.rb
@@ -1,10 +1,8 @@
 #8.9
 #helpers
-
 def pluralize_students(n)
   if n == 1 then "#{n} great student" else "#{n} great students" end
 end
-
 
 # 8.6
 @width = 50
@@ -26,6 +24,31 @@ end
   November: 0,
   December: 8
 }
+
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1.  Input the students"
+    puts "2.  Show the students"
+    puts "9.  Exit"  #9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print_student_list(students)
+      print_footer(students)
+    when "9"
+      exit  #this will cause the program to terminate
+    else
+      puts "I don't know what you meant, please try again"
+    end
+  end
+end
 
 # 8.5
 def create_new_student
@@ -204,8 +227,6 @@ def print_footer(students)
   end
 end
 
-#call the methods
-@students = input_students
-print_header 
-print_student_list(@students) 
-print_footer(@students)
+#call the interactive menu
+interactive_menu
+
